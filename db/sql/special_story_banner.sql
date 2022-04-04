9d6aae93dee374ba311783bb4c9965fbb3396cff
bot0126 <100090048+bot0126@users.noreply.github.com>
2022-04-05 00:55:18 +0800

    202202191541

---
+++ db/sql/special_story_banner.sql
@@ -0,0 +1,5 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE 'special_story_banner' ('id' INTEGER NOT NULL, 'story_group_id' INTEGER NOT NULL, 'start_time' TEXT NOT NULL, 'end_time' TEXT NOT NULL, PRIMARY KEY('id'));
+INSERT INTO `special_story_banner` VALUES (/*id*/1,/*story_group_id*/9002,/*start_time*/"2021-01-30 05:00:00",/*end_time*/"2021-02-28 11:59:59");
+COMMIT;
