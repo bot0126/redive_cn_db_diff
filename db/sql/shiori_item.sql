e077079772c2ebd94b306ab4646f0a9ecefefae6
bot0126 <100090048+bot0126@users.noreply.github.com>
2022-04-05 02:21:14 +0800

    202203011658

--- db/sql/shiori_item.sql
+++ db/sql/shiori_item.sql
@@ -1,4 +1,5 @@
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
 CREATE TABLE 'shiori_item' ('event_id' INTEGER NOT NULL, 'unit_material_id_1' INTEGER NOT NULL, 'unit_material_id_2' INTEGER NOT NULL, PRIMARY KEY('event_id'));
+INSERT INTO `shiori_item` VALUES (/*event_id*/20001,/*unit_material_id_1*/31012,/*unit_material_id_2*/31038);
 COMMIT;
