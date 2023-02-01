252626d861039e583c4c6ad2c752a96efb67e2f6
bot0126 <100090048+bot0126@users.noreply.github.com>
2023-02-01 10:37:52 +0000

    202302011823

--- db/sql/chara_e_ticket_data.sql
+++ db/sql/chara_e_ticket_data.sql
@@ -2,5 +2,5 @@ PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
 CREATE TABLE 'chara_e_ticket_data' ('ticket_id' INTEGER NOT NULL, 'start_time' TEXT NOT NULL, 'end_time' TEXT NOT NULL, 'jewel_store_id' INTEGER NOT NULL, PRIMARY KEY('ticket_id'));
 INSERT INTO `chara_e_ticket_data` VALUES (/*ticket_id*/27001,/*start_time*/"2022/01/28 9:00:00",/*end_time*/"2022/02/25 4:59:59",/*jewel_store_id*/10028);
-INSERT INTO `chara_e_ticket_data` VALUES (/*ticket_id*/27002,/*start_time*/"2023/02/15 11:00:00",/*end_time*/"2023/2/25 23:59:59",/*jewel_store_id*/33);
+INSERT INTO `chara_e_ticket_data` VALUES (/*ticket_id*/27002,/*start_time*/"2023/02/15 11:00:00",/*end_time*/"2023/2/25 23:59:59",/*jewel_store_id*/10033);
 COMMIT;
