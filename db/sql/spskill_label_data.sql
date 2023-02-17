97b81667d86836f4326f6cf5c971d47499e11047
bot0126 <100090048+bot0126@users.noreply.github.com>
2023-02-17 08:00:11 +0000

    202302171527

--- db/sql/spskill_label_data.sql
+++ db/sql/spskill_label_data.sql
@@ -1,5 +1,5 @@
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
 CREATE TABLE 'spskill_label_data' ('unit_id' INTEGER NOT NULL, 'normal_label_text' TEXT NOT NULL, 'sp_label_text' TEXT NOT NULL, PRIMARY KEY('unit_id'));
-INSERT INTO `spskill_label_data` VALUES (/*unit_id*/115801,/*normal_label_text*/"通常時",/*sp_label_text*/"変身中");
+INSERT INTO `spskill_label_data` VALUES (/*unit_id*/115801,/*normal_label_text*/"通常时",/*sp_label_text*/"变身中");
 COMMIT;
