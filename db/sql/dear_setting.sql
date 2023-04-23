881abb7b29212ecd87ae6cedba9d360ad3d6ebd2
bot0126 <100090048+bot0126@users.noreply.github.com>
2023-04-23 10:05:56 +0000

    202304231752

--- db/sql/dear_setting.sql
+++ db/sql/dear_setting.sql
@@ -2,7 +2,7 @@ PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
 CREATE TABLE 'dear_setting' ('event_id' INTEGER NOT NULL, 'system_name' TEXT NOT NULL, 'tutorial_quest_id' INTEGER NOT NULL, 'tutorial_chara_index' INTEGER NOT NULL, 'tutorial_story_id' INTEGER NOT NULL, PRIMARY KEY('event_id'));
 INSERT INTO `dear_setting` VALUES (/*event_id*/10040,/*system_name*/"信赖度",/*tutorial_quest_id*/10067103,/*tutorial_chara_index*/1,/*tutorial_story_id*/6040101);
-INSERT INTO `dear_setting` VALUES (/*event_id*/10042,/*system_name*/"信赖度",/*tutorial_quest_id*/10069103,/*tutorial_chara_index*/1,/*tutorial_story_id*/6042101);
+INSERT INTO `dear_setting` VALUES (/*event_id*/10042,/*system_name*/"信赖度",/*tutorial_quest_id*/10042103,/*tutorial_chara_index*/1,/*tutorial_story_id*/6042101);
 INSERT INTO `dear_setting` VALUES (/*event_id*/10043,/*system_name*/"信赖度",/*tutorial_quest_id*/10043102,/*tutorial_chara_index*/1,/*tutorial_story_id*/6019101);
 INSERT INTO `dear_setting` VALUES (/*event_id*/10044,/*system_name*/"信赖度",/*tutorial_quest_id*/10044103,/*tutorial_chara_index*/1,/*tutorial_story_id*/6044101);
 INSERT INTO `dear_setting` VALUES (/*event_id*/10047,/*system_name*/"信赖度",/*tutorial_quest_id*/10047103,/*tutorial_chara_index*/1,/*tutorial_story_id*/6023101);
