a9fc204b96a686d8edea86bc034ec2243dea17e8
bot0126 <100090048+bot0126@users.noreply.github.com>
2023-08-11 11:23:10 +0000

    10002200-202308081714

--- db/sql/clan_invite_level_group.sql
+++ db/sql/clan_invite_level_group.sql
@@ -40,5 +40,6 @@ INSERT INTO `clan_invite_level_group` VALUES (/*level_group_id*/36,/*team_level_
 INSERT INTO `clan_invite_level_group` VALUES (/*level_group_id*/37,/*team_level_from*/191,/*team_level_to*/195);
 INSERT INTO `clan_invite_level_group` VALUES (/*level_group_id*/38,/*team_level_from*/196,/*team_level_to*/200);
 INSERT INTO `clan_invite_level_group` VALUES (/*level_group_id*/39,/*team_level_from*/201,/*team_level_to*/205);
-INSERT INTO `clan_invite_level_group` VALUES (/*level_group_id*/40,/*team_level_from*/206,/*team_level_to*/-1);
+INSERT INTO `clan_invite_level_group` VALUES (/*level_group_id*/40,/*team_level_from*/206,/*team_level_to*/210);
+INSERT INTO `clan_invite_level_group` VALUES (/*level_group_id*/41,/*team_level_from*/211,/*team_level_to*/-1);
 COMMIT;
