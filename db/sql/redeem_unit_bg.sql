2a98103e99237dd0d40564eef25030b94535e111
bot0126 <100090048+bot0126@users.noreply.github.com>
2023-10-11 13:47:10 +0000

    10002200-202309261520

--- db/sql/redeem_unit_bg.sql
+++ db/sql/redeem_unit_bg.sql
@@ -1,4 +1,5 @@
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
 CREATE TABLE 'redeem_unit_bg' ('unit_id' INTEGER NOT NULL, 'bg_id' INTEGER NOT NULL, PRIMARY KEY('unit_id'));
+INSERT INTO `redeem_unit_bg` VALUES (/*unit_id*/118501,/*bg_id*/530700);
 COMMIT;
--- db/sql/room_exclusive_condition.sql
+++ db/sql/room_exclusive_condition.sql
@@ -1,5 +1,5 @@
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
 CREATE TABLE 'room_exclusive_condition' ('id' INTEGER NOT NULL, 'unit_id' INTEGER NOT NULL, 'room_item_id' INTEGER NOT NULL, 'notification' TEXT NOT NULL, PRIMARY KEY('id'));
-INSERT INTO `room_exclusive_condition` VALUES (/*id*/10000001,/*unit_id*/118501,/*room_item_id*/1,/*notification*/"カリンとカリンの机は同時に配置できません。");
+INSERT INTO `room_exclusive_condition` VALUES (/*id*/10000001,/*unit_id*/118501,/*room_item_id*/1,/*notification*/"不能同时配置花凛和花凛的桌子。");
 COMMIT;
