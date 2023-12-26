091617c345a69a249b05b6a7f282fe00ef89973e
bot0126 <100090048+bot0126@users.noreply.github.com>
2023-12-26 10:49:51 +0000

    202312261832

--- db/sql/campaign_freegacha_data.sql
+++ db/sql/campaign_freegacha_data.sql
@@ -80,4 +80,5 @@ INSERT INTO `campaign_freegacha_data` VALUES (/*id*/76,/*campaign_id*/22,/*gacha
 INSERT INTO `campaign_freegacha_data` VALUES (/*id*/77,/*campaign_id*/22,/*gacha_id*/50172);
 INSERT INTO `campaign_freegacha_data` VALUES (/*id*/78,/*campaign_id*/22,/*gacha_id*/30173);
 INSERT INTO `campaign_freegacha_data` VALUES (/*id*/79,/*campaign_id*/23,/*gacha_id*/30173);
+INSERT INTO `campaign_freegacha_data` VALUES (/*id*/80,/*campaign_id*/23,/*gacha_id*/30174);
 COMMIT;
