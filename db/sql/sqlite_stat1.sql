d186e66c5a83b44980f35b97cb8946cf6f614e41
bot0126 <100090048+bot0126@users.noreply.github.com>
2024-08-30 01:58:37 -0400

    10002200-202407021538

--- db/sql/sqlite_stat1.sql
+++ db/sql/sqlite_stat1.sql
@@ -198,8 +198,8 @@ INSERT INTO `sqlite_stat1` VALUES (/*tbl*/"secret_dungeon_emblem_mission",/*idx*
 INSERT INTO `sqlite_stat1` VALUES (/*tbl*/"grand_arena_daily_rank_reward",/*idx*/NULL,/*stat*/"15");
 INSERT INTO `sqlite_stat1` VALUES (/*tbl*/"room_effect",/*idx*/NULL,/*stat*/"29");
 INSERT INTO `sqlite_stat1` VALUES (/*tbl*/"room_chat_info",/*idx*/NULL,/*stat*/"37");
-INSERT INTO `sqlite_stat1` VALUES (/*tbl*/"unit_unique_equipment",/*idx*/"unit_unique_equipment_0_unit_id",/*stat*/"160 1");
-INSERT INTO `sqlite_stat1` VALUES (/*tbl*/"unit_unique_equipment",/*idx*/"sqlite_autoindex_unit_unique_equipment_1",/*stat*/"160 1 1");
+INSERT INTO `sqlite_stat1` VALUES (/*tbl*/"unit_unique_equipment",/*idx*/"unit_unique_equipment_0_unit_id",/*stat*/"157 1");
+INSERT INTO `sqlite_stat1` VALUES (/*tbl*/"unit_unique_equipment",/*idx*/"sqlite_autoindex_unit_unique_equipment_1",/*stat*/"157 1 1");
 INSERT INTO `sqlite_stat1` VALUES (/*tbl*/"unit_skill_data",/*idx*/NULL,/*stat*/"1750");
 INSERT INTO `sqlite_stat1` VALUES (/*tbl*/"mission_category_icon",/*idx*/NULL,/*stat*/"38");
 INSERT INTO `sqlite_stat1` VALUES (/*tbl*/"ccc_scenario",/*idx*/"ccc_scenario_0_ccc_scenario_id",/*stat*/"1000 200");
@@ -274,7 +274,7 @@ INSERT INTO `sqlite_stat1` VALUES (/*tbl*/"odds_name_data",/*idx*/NULL,/*stat*/"
 INSERT INTO `sqlite_stat1` VALUES (/*tbl*/"hatsune_series_gacha_reference",/*idx*/NULL,/*stat*/"3");
 INSERT INTO `sqlite_stat1` VALUES (/*tbl*/"hatsune_special_boss_ticket_count",/*idx*/NULL,/*stat*/"1");
 INSERT INTO `sqlite_stat1` VALUES (/*tbl*/"sekai_boss_mode",/*idx*/NULL,/*stat*/"12");
-INSERT INTO `sqlite_stat1` VALUES (/*tbl*/"gacha_data",/*idx*/"gacha_data_0_exchange_id",/*stat*/"706 4");
+INSERT INTO `sqlite_stat1` VALUES (/*tbl*/"gacha_data",/*idx*/"gacha_data_0_exchange_id",/*stat*/"707 4");
 INSERT INTO `sqlite_stat1` VALUES (/*tbl*/"room_character_skin_color",/*idx*/NULL,/*stat*/"247");
 INSERT INTO `sqlite_stat1` VALUES (/*tbl*/"room_item_detail",/*idx*/"room_item_detail_0_lvup_trigger_type_2_1_lvup_trigger_id_2",/*stat*/"607 607 607");
 INSERT INTO `sqlite_stat1` VALUES (/*tbl*/"room_item_detail",/*idx*/"room_item_detail_0_lvup_trigger_type_1_lvup_trigger_id",/*stat*/"607 304 304");
